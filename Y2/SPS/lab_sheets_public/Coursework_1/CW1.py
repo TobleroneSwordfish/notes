7f6a6efbc1d38a641b6488eff50d132943d239c5
TobleroneSwordfish <jonathan.hall859@gmail.com>
2019-02-20 02:34:28 +0000

    CW1

--- Y2/SPS/lab_sheets_public/Coursework_1/CW1.py
+++ Y2/SPS/lab_sheets_public/Coursework_1/CW1.py
@@ -1,6 +1,7 @@
 from __future__ import print_function # to avoid issues between Python 2 and 3 printing
 
 import numpy as np
+import sys
 from scipy import stats
 from skimage import data, io, color, transform, exposure
 from pprint import pprint
@@ -9,19 +10,78 @@ from mpl_toolkits.mplot3d import Axes3D
 from utilities import load_points_from_file
 from utilities import view_data_segments
 
-
-#%matplotlib inline
 # notebook
 import matplotlib.pylab as pylab
 pylab.rcParams['figure.figsize'] = (32.0, 24.0)
 pylab.rcParams['font.size'] = 24
 
-print(load_points_from_file("./train/basic_1.csv"))
+segmentLength = 20
 
 #does least squares
-def leastSquares(X, Y):
+def leastSquares(X, Y, n):
     Y = np.matrix(Y).T
-    Xones = np.matrix([np.ones(X.size), X]).T
-    ans = np.linalg.inv(Xones.T.dot(Xones)).dot(Xones.T).dot(Y)
+    XMatrix = np.matrix([nDegree(X, i) for i in range(0, n)]).T
+    ans = np.linalg.inv(XMatrix.T.dot(XMatrix)).dot(XMatrix.T).dot(Y)
     return ans
 
+#returns X with every element raised to n, kinda pointless but eh
+def nDegree(X, n):
+   return [x ** n for x in X]
+
+#evaluates each point in X for the polynomial represented by coefficients
+def evalArray(X, coefficients):
+    return [eval(x, coefficients) for x in X]
+
+#evaluates the data point x with a polynomial represented by coefficients
+def eval(x, coefficients):
+    return sum([c * (x ** i) for i, c in enumerate(coefficients)])
+
+def split(array, n):
+    if len(array) == n:
+        return [array]
+    return [array[:n]] + split(array[n:len(array)], n)
+
+#returns the total squared error between the original data points and the estimated ones
+def error(Y, estY):
+    range = abs(max(Y) - min(Y))
+    return sum([(y - y1)/range * (y - y1)/range for y, y1 in zip(Y, estY)])
+
+data = load_points_from_file(sys.argv[1])
+
+dataX = split(data[0], segmentLength)
+dataY = split(data[1], segmentLength)
+
+def getError(X, Y, n):
+    poly = leastSquares(X, Y, n)
+    evalY = evalArray(X, poly)
+    return error(Y, evalY)
+
+def getPolynomial(X, Y, lastError, n = 2):
+    poly = leastSquares(X, Y, n)
+    evalY = evalArray(X, poly)
+    e = error(Y, evalY)
+    print ("Error at degree {} is {} where the polynomial is {}".format(n - 1, e, poly))
+    if ((lastError - e) > .01):
+        return getPolynomial(X, Y, e, n + 1)
+    return leastSquares(X, Y, n - 1) #ehhhh, no worries
+
+def truncate(n, decimals=0):
+    multiplier = 10 ** decimals
+    return int(n * multiplier) / multiplier
+
+def printPoly(coefficients):
+    return " + ".join(["{}x^{}".format(truncate(c, 3), i) for i, c in enumerate(coefficients)])
+    
+
+# As = [leastSquares(x, y, 3) for x, y in zip(dataX, dataY)]
+As = [getPolynomial(X, Y, getError(X, Y, 1)) for X, Y in zip(dataX, dataY)]
+
+print([printPoly(c.A1) for c in As])
+
+for i, x in enumerate(dataX):
+    plt.plot(x, evalArray(x, As[i].A1))
+
+
+[plt.scatter(x, y) for x, y in zip(dataX, dataY)]
+
+plt.show()
